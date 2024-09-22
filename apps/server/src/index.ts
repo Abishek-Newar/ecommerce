d270821463e2f8c805d5a40ce6a741bfb55dc0bf
Hemant Singh <honeyaps12345@gmail.com>
2024-09-22 15:15:19 +0530

    changes for deployment

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -33,10 +33,6 @@ dbconnection();
 sequelize.authenticate();
 app.use("/v1", createRouter());
 
-app.get("/", (req, res) => {
-  res.send("it works!");
-})
-
 app.listen(port, () => {
   console.log(`APP listening on port:${port}`);
 });
